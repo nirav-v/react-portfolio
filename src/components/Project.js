7366bed2dcc59d904321e013ade05d07fb0f0297
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-28 18:27:28 -0700

    added images folder and passing in image and all other project variables as props when rendering a project card componenet

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,24 @@
-function Project({title, link, description}) {
-    const styles = {
-     card: {
-         width: '18rem',
-         padding: 15,
-         marginTop: 20
-     }
-    }
+
+function Project({ title, image, link, description }) {
+  const styles = {
+    card: {
+      width: "18rem",
+      padding: 15,
+      marginTop: 20,
+    },
+  };
   return (
-    
-      <div className="card" style={styles.card}>
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          <a href="#github" className="btn btn-primary">
-            Github
-          </a>
-        </div>
+    <div className="card" style={styles.card}>
+        <img src = {image}
+        class="card-img-top" alt="pets"/>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={link} className="btn btn-primary">
+          Github
+        </a>
       </div>
-   
+    </div>
   );
 }
 
--- src/components/pages/PortfolioContainer.js
+++ src/components/pages/PortfolioContainer.js
@@ -1,9 +1,13 @@
 import Header from "../Header";
 import Project from "../Project";
+import adoptOwnerImage from '../../images/adopt-an-owner.png'
+
 
 const projects = {
     adoptOwner: {
         title: "Adopt-an-Owner",
+        description: "Switching the roles of the pet search market",
+        github: "https://github.com/nirav-v/adopt-an-owner"
     }
 }
 
@@ -11,7 +15,7 @@ function Portfolio() {
     return ( 
     <div>
     <Header />
-    <Project title={projects.adoptOwner.title} />
+    <Project title={projects.adoptOwner.title} image={adoptOwnerImage} description={projects.adoptOwner.description} link={projects.adoptOwner.github}/>
     </div>
     );
 }
