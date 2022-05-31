c031904d894bab5a7d88758b3af6b47b9f1cfa78
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-31 16:01:05 -0700

    added profile pic to about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,9 +1,22 @@
+import profilePic from '../../images/profile-pic.jpg'
+
+const styles = {
+    bio: {
+        backgroundColor: "blue"
+    },
+    headshot: {
+        borderRadius: 100,
+    }
+
+}
+
 function AboutMe() {
   return (
     <div>
       <h1>About Me</h1>
+      <img src= {profilePic} alt={"profile headshot"} style={styles.headshot}/>
       <section>
-        <p className="bio container-sm">
+        <p className="bio container-fluid" style={styles.bio}>
           I am a full stack web developer residing in San Diego, CA. I am
           fascinated by how the web works, and am passionate about ensuring the
           best possible user experience in every project that I am involved
