f0266628395c2497cb380d9637e45b9a3c497461
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-31 18:48:34 -0700

    added my name to the header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
-import AboutMe from './pages/AboutMe'
-import Portfolio from './pages/PortfolioContainer';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume'
-import NavBar from './NavBar';
-import Footer from './Footer';
-import '../styles/Main.css'
+import React, { useState } from "react";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/PortfolioContainer";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import "../styles/Main.css";
 // Main is the 'smart' component that determines which pages to render
 
-
 function Main() {
-// declare a state variable for the current page
-const [currentPage, setCurrentPage] = useState('About Me');
+  // declare a state variable for the current page
+  const [currentPage, setCurrentPage] = useState("About Me");
 
-// conditionally render AboutMe, Portfolio, Contact, or Resume component based on switch case for the currentPage state variable
+  // conditionally render AboutMe, Portfolio, Contact, or Resume component based on switch case for the currentPage state variable
   const renderPage = () => {
     switch (currentPage) {
-      case 'Portfolio':
+      case "Portfolio":
         return <Portfolio />;
-      case 'Contact':
+      case "Contact":
         return <Contact />;
-      case 'About Me':
+      case "About Me":
         return <AboutMe />;
-      case 'Resume':
+      case "Resume":
         return <Resume />;
       default:
         return <Portfolio />;
@@ -30,22 +29,18 @@ const [currentPage, setCurrentPage] = useState('About Me');
   };
 
   // switch the current page state variable based on the page name it is passed
-    const switchPage = (page) => setCurrentPage(page);
+  const switchPage = (page) => setCurrentPage(page);
 
-    return ( 
-        <div className='main'>
-         
-             <NavBar currentPage={currentPage} handlePageChange={switchPage}/>
-        
-           {renderPage()}
-           <Footer />
-        </div>
-     );
+  return (
+    <div className="main">
+      <header>
+        <NavBar currentPage={currentPage} handlePageChange={switchPage} />
+        <h1>{"Nirav Venkatesan"}</h1>
+      </header>
+      {renderPage()}
+      <Footer />
+    </div>
+  );
 }
-
-
-
-
-
 
 export default Main;
