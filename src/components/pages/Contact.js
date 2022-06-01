e14fc4edc539729f0be0728fb8075911761b8192
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-31 17:24:32 -0700

    adding to resume page and styling contact list

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,14 @@
+const styles = {
+list: {
+  listStyle: 'none'
+}
+}
+
 function Contact() {
   return (
     <div>
-      <ul className="contact-info">
-        <li>Phone: 408-425-2694</li>
+      <ul className="contact-info" style={styles.list}>
+        <li >Phone: 408-425-2694</li>
         <li>
           <a href="#email">Email: niravvenkatesan@gmail.com</a>
         </li>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,32 @@
-import niravResume from "../../(4_17_22) NiravVenkatesan_Resume .pdf"
+import "../../styles/Resume.css";
+
+import niravResume from "../../(4_17_22) NiravVenkatesan_Resume .pdf";
 
 function Resume() {
-    return (  
-        <div>
-            
-            <h2>Check out <a href={niravResume}>my resume</a> for more info</h2>
-        </div>
-    );
+  return (
+    <div className="resume-page container">
+      <h2 className="resume-link">
+        {" "}
+        <a href={niravResume}>see my resume here</a>{" "}
+      </h2>
+      <h3>Front-end Skills</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h3>Back-end Skills</h3>
+      <ul>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>GraphQl, Apollo Server</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
