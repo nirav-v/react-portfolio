52e9de916723282ed88d90066e60cb1a6eaccf59
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-06-23 09:15:55 -0700

    pull from remote and remove commented form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -21,28 +21,6 @@ list: {
 function Contact() {
   return (
 
-
-
-
-//     <div>
-//   <form>
-//     <label htmlFor="fname">First Name</label>
-//     <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-
-//     <label htmlFor="lname">Last Name</label>
-//     <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-
-//     <label htmlFor="email">Email Address</label>
-//     <input type="email" id="email" name="email" placeholder="Your email address.."/>
-
-
-//     <label htmlFor="subject">Subject</label>
-//     {/* <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea> */}
-
-//     <input type="submit" value="Submit"/>
-
-//   </form>
-// </div>
 <div>      
 
 <Grid
