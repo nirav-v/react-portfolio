c17d02055427779d5ebab6eb7886e13619123fc8
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-09-10 10:53:41 -0700

    removed unused card and styles code

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,17 +8,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 function Project({ title, imageSrc, imageAlt, deployedLink, github, description })  {
-  const styles = {
-    card: {
-      width: "18rem",
-      padding: 20,
-      marginTop: 60,
-      color: "black",
-      fontFamily: 'Times New Roman, serif',
-      backgroundColor: '#c6e7f5'
-      
-    }
-  };
+
   return (
 
  <Card sx={{ maxWidth: 375 }}>
@@ -42,17 +32,6 @@ function Project({ title, imageSrc, imageAlt, deployedLink, github, description 
       </CardActions>
     </Card>
 
-    // <div className="card" style={styles.card}>
-    //     <img src = {imageSrc}
-    //     className="card-img-top" alt={imageAlt}/>
-    //   <div className="card-body">
-    //     <a className="project-link" href={deployedLink}  target={"_blank"} rel={"noreferrer"}><h5 className="card-title">{title}</h5></a>
-    //     <p className="card-text">{description}</p>
-    //     <a className='github-link' href={github}  target={"_blank"} rel={"noreferrer"} >
-    //       See Github Repository
-    //     </a>
-    //   </div>
-    // </div>
   );
 }
 
