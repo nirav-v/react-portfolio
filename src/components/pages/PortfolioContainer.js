b49a88439d58a6f71089ab6d79c84b84fbf40a9b
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-11-25 14:03:37 -0800

    added dwRugs project to images and portfolio component as an additional property inside the projects object.

--- src/components/pages/PortfolioContainer.js
+++ src/components/pages/PortfolioContainer.js
@@ -3,23 +3,24 @@ import adoptOwnerImage from "../../images/adopt-an-owner.png";
 import golfapediaImage from "../../images/golfapedia.png";
 import secureRandoImage from "../../images/secure-rando.png";
 import bowelMvmtImage from "../../images/bowel-mvmt.jpg";
-import javaNewsImage from "../../images/javaNews.png"
-import netflixReviewsImage from "../../images/netflix-reviews.png"
-import { Grid} from "@mui/material";
+import javaNewsImage from "../../images/javaNews.png";
+import netflixReviewsImage from "../../images/netflix-reviews.png";
+import dwRugsImage from "../../images/dw-rugs.png";
+import { Grid } from "@mui/material";
 // import { Item} from "@mui/material";
-
-
 
 const projects = {
   toiletTracker: {
     title: "Restroom-Router",
-    description: "Crowdsourcing the most transparent and up to date public restroom information. MERN stack application built with React, Apollo server, GraphQL, and MongoDB. MapBox API used for displaying restrooms on map.",
+    description:
+      "Crowdsourcing the most transparent and up to date public restroom information. MERN stack application built with React, Apollo server, GraphQL, and MongoDB. MapBox API used for displaying restrooms on map.",
     deployedLink: "https://restroom--router.herokuapp.com/",
     github: "https://github.com/nirav-v/restroom-router",
   },
   adoptOwner: {
     title: "Adopt-an-Owner",
-    description: "Switching the roles of the pet adoption market by allowing adoption agencies to find new owners through their online profile. Developed using RESTful routes, following MVC paradigm. User data stored and queried using MySQL database and Sequelize ORM. Front end built with HTML, CSS, JavaScript, and Handlebars",
+    description:
+      "Switching the roles of the pet adoption market by allowing adoption agencies to find new owners through their online profile. Developed using RESTful routes, following MVC paradigm. User data stored and queried using MySQL database and Sequelize ORM. Front end built with HTML, CSS, JavaScript, and Handlebars",
     deployedLink: "https://radiant-wave-08847.herokuapp.com/",
     github: "https://github.com/nirav-v/adopt-an-owner",
   },
@@ -27,46 +28,53 @@ const projects = {
     title: "Golfapedia",
     description:
       "Displaying local weather and nearby golf courses when users search a city. Data obtained from Google Maps API and Open Weather API",
-      deployedLink: "https://nirav-v.github.io/golfapedia/",
+    deployedLink: "https://nirav-v.github.io/golfapedia/",
     github: "https://github.com/nirav-v/golfapedia",
   },
   javaNews: {
     title: "Fake News Java API",
-    description: "Social media based news application that allows users to post links to news articles, comment on posts, and upvote posts. Server created using Java with Spring Framework",
+    description:
+      "Social media based news application that allows users to post links to news articles, comment on posts, and upvote posts. Server created using Java with Spring Framework",
     deployedLink: "https://java-fake-news-api.herokuapp.com/",
-    github: "https://github.com/nirav-v/fake-news-java-api"
+    github: "https://github.com/nirav-v/fake-news-java-api",
   },
-  netflixReviews:{
+  netflixReviews: {
     title: "Netflix Review Board",
-    description: "Pulling from a database of roughly 9000 Netflix movies and shows, this site allows a user to leave their public review for any of them. Backend built with Python and Flask Server. MySQL database with SqlAlchemy ORM. Frontend built with HTML, CSS, JavaScript",
+    description:
+      "Pulling from a database of roughly 9000 Netflix movies and shows, this site allows a user to leave their public review for any of them. Backend built with Python and Flask Server. MySQL database with SqlAlchemy ORM. Frontend built with HTML, CSS, JavaScript",
     deployedLink: "https://netflix-public-review-board.herokuapp.com/",
-    github: "https://github.com/nirav-v/netflix-show-reviews"
+    github: "https://github.com/nirav-v/netflix-show-reviews",
   },
-    secureRando: {
+  secureRando: {
     title: "Secure Rando",
     description:
       "Generate a random password based on the criteria the user gets prompted to select. HTML, CSS, and JavaScript",
-      deployedLink: "https://nirav-v.github.io/secure-rando/",
+    deployedLink: "https://nirav-v.github.io/secure-rando/",
     github: "https://github.com/nirav-v/secure-rando",
-  }
-
+  },
+  dwRugs: {
+    title: "DW Rugs",
+    description:
+      "A static website with a gallery page that I developed for my close friend's custom rug making business. I was able to use my front-end development skills to promote a small business and showcase their work.",
+    deployedLink: "https://nirav-v.github.io/dw-rugs/index.html",
+    github: "https://github.com/nirav-v/dw-rugs",
+  },
 };
 
 function Portfolio() {
   return (
     <div className="projects container d-flex p-4">
-
-<Grid container spacing={2}>
-  <Grid item xs={12} sm={6} >
-     <Project
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Project
             title={projects.toiletTracker.title}
             imageSrc={bowelMvmtImage}
             description={projects.toiletTracker.description}
             deployedLink={projects.toiletTracker.deployedLink}
             github={projects.toiletTracker.github}
           />
-  </Grid>
-   <Grid item xs={12} sm={6} >
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Project
             title={projects.netflixReviews.title}
             imageSrc={netflixReviewsImage}
@@ -74,26 +82,26 @@ function Portfolio() {
             deployedLink={projects.netflixReviews.deployedLink}
             github={projects.netflixReviews.github}
           />
-  </Grid>
-  <Grid item xs={12} sm={6} >
-     <Project
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Project
             title={projects.adoptOwner.title}
             imageSrc={adoptOwnerImage}
             description={projects.adoptOwner.description}
             deployedLink={projects.adoptOwner.deployedLink}
             github={projects.adoptOwner.github}
           />
-  </Grid>
-  <Grid item xs={12} sm={6} >
-   <Project
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Project
             title={projects.golfapedia.title}
             imageSrc={golfapediaImage}
             description={projects.golfapedia.description}
             deployedLink={projects.golfapedia.deployedLink}
             github={projects.golfapedia.github}
           />
-  </Grid>
-  <Grid item xs={12} sm={6} >
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Project
             title={projects.secureRando.title}
             imageSrc={secureRandoImage}
@@ -101,8 +109,8 @@ function Portfolio() {
             deployedLink={projects.secureRando.deployedLink}
             github={projects.secureRando.github}
           />
-  </Grid>
-  <Grid item xs={12} sm={6} >
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Project
             title={projects.javaNews.title}
             imageSrc={javaNewsImage}
@@ -110,11 +118,8 @@ function Portfolio() {
             deployedLink={projects.javaNews.deployedLink}
             github={projects.javaNews.github}
           />
-  </Grid>
- 
-</Grid>
-
-     
+        </Grid>
+      </Grid>
     </div>
   );
 }
